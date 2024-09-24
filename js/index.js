c35970cd45af4d721b01d452c8c80a47310ed783
@maptaul <maptaulislam1@gmail.com>
2024-09-24 22:10:00 +0600

    result

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("connected.........");
+const calculationButton = document.getElementById("calculate");
+calculationButton.addEventListener("click", function () {
+  const income = parseFloat(document.getElementById("income").value);
+  const software = parseFloat(document.getElementById("software").value);
+  const courses = parseFloat(document.getElementById("courses").value);
+  const internet = parseFloat(document.getElementById("internet").value);
+
+  const totalExpense = software + courses + internet;
+  const Balance = income - totalExpense;
+
+  const totalExpenseElement = document.getElementById("total-expenses");
+  totalExpenseElement.innerText = totalExpense;
+
+  const totalBalance = document.getElementById("balance");
+  totalBalance.innerText = Balance;
+});
