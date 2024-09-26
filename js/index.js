7138372b05c4d1d7990e1f72820f8d169669ec51
@maptaul <maptaulislam1@gmail.com>
2024-09-26 10:45:23 +0600

    history

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ calculationButton.addEventListener("click", function () {
   const internet = parseFloat(document.getElementById("internet").value);
 
   const totalExpense = software + courses + internet;
-  const Balance = income - totalExpense;
+  const balance = income - totalExpense;
 
   const totalExpenseElement = document.getElementById("total-expenses");
   totalExpenseElement.innerText = totalExpense.toFixed(2);
 
   const totalBalance = document.getElementById("balance");
-  totalBalance.innerText = Balance.toFixed(2);
+  totalBalance.innerText = balance.toFixed(2);
 
   const result = document.getElementById("results");
   result.classList.remove("hidden");
@@ -38,4 +38,43 @@ calculateSavingButton.addEventListener("click", function () {
 
   const remainingElement = document.getElementById("remaining-balance");
   remainingElement.innerText = remainingBalance.toFixed(2);
+});
+
+// const calculateSavingButton = document.getElementById("calculate-savings");
+// calculateSavingButton.addEventListener("click", function () {
+//   const income = parseFloat(document.getElementById("income").value);
+//   const software = parseFloat(document.getElementById("software").value);
+//   const courses = parseFloat(document.getElementById("courses").value);
+//   const internet = parseFloat(document.getElementById("internet").value);
+//   const savingParentage = parseFloat(document.getElementById("savings").value);
+
+//   const savingAmount = (savingParentage * balance) / 100;
+
+//   const remainingBalance = balance - savingAmount;
+
+//   const savingElement = document.getElementById("savings-amount");
+//   savingElement.innerText = savingAmount.toFixed(2);
+
+//   const remainingElement = document.getElementById("remaining-balance");
+//   remainingElement.innerText = remainingBalance.toFixed(2);
+// });
+
+const historyTab = document.getElementById("history-tab");
+const assistantTab = document.getElementById("assistant-tab");
+historyTab.addEventListener("click", function () {
+  historyTab.classList.add(
+    "text-white",
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600"
+  );
+  historyTab.classList.remove("text-gray-600");
+  assistantTab.classList.remove(
+    "text-white",
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600"
+  );
+  assistantTab.classList.add("text-gray-600");
+  document.getElementById("expense-form").classList.add("hidden");
 });
